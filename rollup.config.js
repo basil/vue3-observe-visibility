69fd92c6af0d38186b94ddc5d44b68e9e57c0c3b
UxieVerity <1351117103@qq.com>
2023-05-21 22:30:27 +0800

    add terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,11 @@
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import terser from "@rollup/plugin-terser";
 
 const config = [
   {
     input: "src/index.ts",
     output: [
-      {
-        file: "dist/index.es.js",
-        format: "es",
-      },
       {
         file: "dist/index.browser.js",
         format: "iife",
@@ -27,6 +24,25 @@ const config = [
         },
         exports: "named",
       },
+    ],
+    plugins: [
+      typescript({
+        tsconfig: "tsconfig.json",
+      }),
+      terser()
+    ],
+    external: ["vue"],
+    watch: {
+      include: "src/**",
+    },
+  },
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/index.es.js",
+        format: "es",
+      }
     ],
     plugins: [
       typescript({
